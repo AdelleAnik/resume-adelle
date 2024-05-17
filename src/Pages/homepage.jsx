2c784e64703e8a87208249f356fae09a3a7aad6a
Adelle Gomes <adelle-anik@hotmail.com>
2024-05-17 14:47:13 -0300

    modified icon, tab name and made it responsive to mobile

--- src/Pages/homepage.jsx
+++ src/Pages/homepage.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import { Box, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography, Avatar } from '@mui/material';
+import { styled, useTheme } from '@mui/material/styles';
+import { Box, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography, Avatar, IconButton, useMediaQuery } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
 import BuildIcon from '@mui/icons-material/Build';
 import WorkIcon from '@mui/icons-material/Work';
@@ -9,8 +10,8 @@ import ComputerIcon from '@mui/icons-material/Computer';
 import Skills from './skills';
 import Experience from './experience';
 import Education from './education';
-import image from '../Images/360_F_89026793_eyw5a7WCQE0y1RHsizu41uhj7YStgvAA.jpg'
-import adelleLogo from '../Images/AdelleGomes.png'
+import image from '../Images/360_F_89026793_eyw5a7WCQE0y1RHsizu41uhj7YStgvAA.jpg';
+import adelleLogo from '../Images/AdelleGomes.png';
 import Technologies from './technologies';
 
 const drawerWidth = 240;
@@ -23,13 +24,13 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    marginLeft: `-${drawerWidth}px`,
+    marginLeft: 0,
     ...(open && {
+      marginLeft: drawerWidth,
       transition: theme.transitions.create('margin', {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
-      marginLeft: 0,
     }),
   }),
 );
@@ -44,11 +45,24 @@ const DrawerStyled = styled(Drawer)(({ theme }) => ({
 }));
 
 const Homepage = () => {
-  const [open] = React.useState(true);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [open, setOpen] = React.useState(!isMobile);
   const [selectedPage, setSelectedPage] = React.useState('Home');
+
+  React.useEffect(() => {
+    setOpen(!isMobile);
+  }, [isMobile]);
+
+  const handleDrawerToggle = () => {
+    setOpen(!open);
+  };
 
   const handleListItemClick = (text) => {
     setSelectedPage(text);
+    if (isMobile) {
+      setOpen(false);
+    }
   };
 
   const renderContent = (page) => {
@@ -65,7 +79,7 @@ const Homepage = () => {
         return (
           <div>
             <Typography variant="h5">
-              Hello! Welcome to Adelle's Resume 
+              Hello! Welcome to Adelle's Resume
             </Typography>
             <img src={image} alt="logo" />
             <Typography variant="h6">
@@ -78,7 +92,16 @@ const Homepage = () => {
 
   return (
     <Box sx={{ display: 'flex' }}>
-      <DrawerStyled variant="persistent" anchor="left" open={open}>
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={handleDrawerToggle}
+        edge="start"
+        sx={{ display: { sm: 'none' }, position: 'fixed', top: 0, left: 0, zIndex: theme.zIndex.drawer + 1 }}
+      >
+        <MenuIcon />
+      </IconButton>
+      <DrawerStyled variant={isMobile ? 'temporary' : 'persistent'} anchor="left" open={open} onClose={handleDrawerToggle}>
         <Box
           sx={{
             display: 'flex',
@@ -110,9 +133,7 @@ const Homepage = () => {
         </List>
       </DrawerStyled>
       <Main open={open}>
-        <Typography paragraph>
-          {renderContent(selectedPage)}
-        </Typography>
+        {renderContent(selectedPage)}
       </Main>
     </Box>
   );
