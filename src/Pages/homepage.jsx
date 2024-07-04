2e9f60bf1582a1237bd371e5b4cb6e31f3673b9b
Adelle Gomes <adelle-anik@hotmail.com>
2024-07-04 11:33:25 -0300

    added footer and first draft of homepage image

--- src/Pages/homepage.jsx
+++ src/Pages/homepage.jsx
@@ -1,36 +1,40 @@
 import * as React from 'react';
+import './homepage.css'; 
 import { styled, useTheme } from '@mui/material/styles';
-import { Box, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography, Avatar, IconButton, useMediaQuery } from '@mui/material';
+import { Box, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography, Avatar, IconButton, Link, useMediaQuery } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
 import BuildIcon from '@mui/icons-material/Build';
 import WorkIcon from '@mui/icons-material/Work';
 import SchoolIcon from '@mui/icons-material/School';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import ComputerIcon from '@mui/icons-material/Computer';
 import Skills from './skills';
 import Experience from './experience';
 import Education from './education';
 import adelleLogo from '../Images/AdelleGomes.png';
 import Technologies from './technologies';
+import home from '../Images/Home.png'
 
-const drawerWidth = 250;
+const drawerWidth = 200;
 
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
   ({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: 0,
-    ...(open && {
-     marginLeft: (drawerWidth/2 ),
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    }),
+    // flexGrow: 1,
+    // padding: theme.spacing(3),
+    // transition: theme.transitions.create('margin', {
+    //   easing: theme.transitions.easing.sharp,
+    //   duration: theme.transitions.duration.leavingScreen,
+    // }),
+    // marginLeft: 0,
+    // ...(open && {
+    //  marginLeft: (drawerWidth/2 ),
+    //   transition: theme.transitions.create('margin', {
+    //     easing: theme.transitions.easing.easeOut,
+    //     duration: theme.transitions.duration.enteringScreen,
+    //   }),
+    // }),
   }),
 );
 
@@ -72,23 +76,27 @@ const Homepage = () => {
         return <Experience />;
       case 'Education':
         return <Education />;
-      case 'Technologies':
+      case 'Contact  Me':
         return <Technologies />;
       default:
         return (
-          <div>
-            <Typography variant="h5">
-            Welcome to My Professional World
-            </Typography>
-                <Typography variant="h6" paddingTop={5}>
-                Hello, I'm Adelle Gomes, a dedicated and innovative software engineer with a rich background in full-stack development and a recent focus on frontend technologies. With a Bachelor’s degree in Environmental Control Technology from UNICAMP and advanced programming training from NBCC, I blend a unique perspective on software solutions with technical proficiency.
-                </Typography>
-                <Typography variant="h6">
-                Over the years, I've contributed to various transformative projects, such as developing a SaaS platform at Profitual Inc. and pioneering generative AI systems at Lateetud. My expertise lies in creating seamless user experiences and scalable software solutions using technologies like JavaScript, React, and Python. I’m passionate about leveraging technology to solve real-world problems, continuously pushing the boundaries of what is possible in digital spaces.
-                </Typography>
-                <Typography variant="h6">
-                Through this website, I invite you to explore my resume. Whether you are a potential employer, a fellow tech enthusiast, or a collaborator, I look forward to connecting and exploring how we can drive technology forward together.
-                </Typography>
+          <div class="container">
+            <img src={home} alt="Snow" style={{width:'100%'}}></img>
+            <div class="top-right">
+              <Typography variant="h5">
+              Welcome to My Professional World
+              </Typography>
+              <Typography variant="h6" paddingTop={5}>
+              Hello, I'm Adelle Gomes, a dedicated and innovative software engineer with a rich background in full-stack development and a recent focus on frontend technologies. With a Bachelor’s degree in Environmental Control Technology from UNICAMP and advanced programming training from NBCC, I blend a unique perspective on software solutions with technical proficiency.
+              </Typography>
+              <Typography variant="h6">
+              Over the years, I've contributed to various transformative projects, such as developing a SaaS platform at Profitual Inc. and pioneering generative AI systems at Lateetud. My expertise lies in creating seamless user experiences and scalable software solutions using technologies like JavaScript, React, and Python. I’m passionate about leveraging technology to solve real-world problems, continuously pushing the boundaries of what is possible in digital spaces.
+              </Typography>
+              <Typography variant="h6">
+              Through this website, I invite you to explore my resume. Whether you are a potential employer, a fellow tech enthusiast, or a collaborator, I look forward to connecting and exploring how we can drive technology forward together.
+              </Typography>
+            </div>
+           
           </div>
         );
     }
@@ -127,7 +135,7 @@ const Homepage = () => {
             { text: 'Skills', icon: <BuildIcon /> },
             { text: 'Experience', icon: <WorkIcon /> },
             { text: 'Education', icon: <SchoolIcon /> },
-            { text: 'Technologies', icon: <ComputerIcon /> },
+            { text: 'Contact Me', icon: <ComputerIcon /> },
           ].map((item) => (
             <ListItem button key={item.text} onClick={() => handleListItemClick(item.text)}>
               <ListItemIcon>{item.icon}</ListItemIcon>
@@ -135,6 +143,14 @@ const Homepage = () => {
             </ListItem>
           ))}
         </List>
+        <Box sx={{ marginTop: 'auto', display: 'flex', justifyContent: 'space-around', padding: theme.spacing(2)  }}>
+          <Link href="https://www.linkedin.com/in/adelle-anik-araujo-gomes/" target="_blank">
+            <LinkedInIcon style={{ color: 'black' }}  />
+          </Link>
+          <Link href="https://github.com/AdelleAnik" target="_blank">
+            <GitHubIcon style={{ color: 'black' }} />
+          </Link>
+        </Box>
       </DrawerStyled>
       <Main open={open}>
         {renderContent(selectedPage)}
