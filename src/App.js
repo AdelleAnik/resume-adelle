64c9d2d88e5aa6c160eb65fa50acfb710448e468
Adelle Gomes <adelle-anik@hotmail.com>
2024-07-06 17:32:58 -0300

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 import Homepage from './Pages/homepage';
+import { initGA, logPageView } from './analytics';
 
 function App() {
+  useEffect(() => {
+    initGA();
+    logPageView();
+  }, []);
+  
   return (
    <Homepage/>
   );
