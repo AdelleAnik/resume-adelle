be4ff9675219bda371ce95ae0df12b93d8460594
Adelle Gomes <adelle-anik@hotmail.com>
2024-07-06 17:11:25 -0300

    updated homepage

--- src/Pages/homepage.jsx
+++ src/Pages/homepage.jsx
@@ -15,6 +15,7 @@ import Experience from './experience';
 import Education from './education';
 import ContactMe from './contactMe'
 import adelleLogo from '../Images/AdelleGomes.png';
+import image from '../Images/360_F_89026793_eyw5a7WCQE0y1RHsizu41uhj7YStgvAA.jpg'
 // import Technologies from './technologies';
 // import home from '../Images/Home.png'
 
@@ -108,12 +109,18 @@ const Homepage = () => {
             <Typography variant="h6" paddingTop={5}>
             Hello, I'm Adelle Gomes, a dedicated and innovative software engineer with a rich background in full-stack development and a recent focus on frontend technologies. With a Bachelor’s degree in Environmental Control Technology from UNICAMP and advanced programming training from NBCC, I blend a unique perspective on software solutions with technical proficiency.
             </Typography>
-            <Typography variant="h6">
+            {/* <Typography variant="h6">
             Over the years, I've contributed to various transformative projects, such as developing a SaaS platform at Profitual Inc. and pioneering generative AI systems at Lateetud. My expertise lies in creating seamless user experiences and scalable software solutions using technologies like JavaScript, React, and Python. I’m passionate about leveraging technology to solve real-world problems, continuously pushing the boundaries of what is possible in digital spaces.
             </Typography>
             <Typography variant="h6">
             Through this website, I invite you to explore my resume. Whether you are a potential employer, a fellow tech enthusiast, or a collaborator, I look forward to connecting and exploring how we can drive technology forward together.
+            </Typography> */}
+            <div>
+            <img src={image}  alt="logo" />
+            <Typography variant="h6">
+             The website is currently under construction, but feel free to take a look around 😉
             </Typography>
+          </div>
           </div>
         </div>
         );
