9259097f19c61895a9eb5198f326424652edccd3
Adelle Gomes <adelle-anik@hotmail.com>
2024-07-09 09:59:38 -0300

    removed non-working code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import React, { useEffect } from 'react';
 import Homepage from './Pages/homepage';
 import { initGA, logPageView } from './analytics';
-import usePageTracking from './usePackageTracking';
 
 function App() {
   useEffect(() => {
     initGA();
     logPageView();
   }, []);
-  usePageTracking();
   return (
    <Homepage/>
   );
