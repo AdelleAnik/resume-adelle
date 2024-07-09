8860be8eaf64859452a05d7906eba2f0bdb3331f
Adelle Gomes <adelle-anik@hotmail.com>
2024-07-09 09:48:35 -0300

    updated google analytics code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React, { useEffect } from 'react';
 import Homepage from './Pages/homepage';
 import { initGA, logPageView } from './analytics';
+import usePageTracking from './usePageTracking';
 
 function App() {
   useEffect(() => {
     initGA();
     logPageView();
   }, []);
-
+  usePageTracking();
   return (
    <Homepage/>
   );
